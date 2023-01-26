484e8fd8f29b4928398edeadc982d8659895a6b4
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-01-26 03:59:47 +0300

    bootstrap added as dependency

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
 	import Header from './components/Header.svelte';
 	import './styles.css';
+	import 'bootstrap/dist/css/bootstrap.min.css';
+	import 'bootstrap/dist/js/bootstrap.min.js';
+	import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 </script>
 
 <Header />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,12 +7,6 @@
 	<title>Home</title>
 	<meta name="description" content="Page description" />
 	<link
-		href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-		rel="stylesheet"
-		integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
-		crossorigin="anonymous"
-	/>
-	<link
 		rel="stylesheet"
 		href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@200;400;700"
 	/>
