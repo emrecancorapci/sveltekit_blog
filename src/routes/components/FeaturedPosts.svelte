69c85e2f6d2db70796e5c735680344de6eaba5a4
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-01-29 05:52:51 +0300

    fetch for component

--- src/routes/components/FeaturedPosts.svelte
+++ src/routes/components/FeaturedPosts.svelte
@@ -1,5 +1,13 @@
 <script>
-	import { posts } from "$lib/data.js";
+	import randImgUrl from '$lib/randImgUrl';
+
+	async function getPosts() {
+		const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+		const posts = await response.json();
+		return posts;
+	}
+
+	let postsPromise = getPosts();
 
 	const imgStyle = (/** @type {string} */ url) => {
 		return `background-image: linear-gradient(to bottom,
@@ -9,55 +17,46 @@
 </script>
 
 <div class="row p-5 justify-content-center">
-	<div class="col-lg-3 col-md-4 mt-md-0 mt-sm-2">
-		<div class="row ratio ratio-1x1 mb-4 rounded rounded-3 shadow-sm">
-			<div class="bg-img rounded rounded-3 shadow" style={imgStyle(posts[0].image)} />
-			<div class="row align-content-end p-4 text-white">
-				<h4 class="fw-bold tx-shadow-title">{posts[0].title}</h4>
-				<p class="tx-shadow-title">
-					{posts[0].content}
-				</p>
+	{#await postsPromise}
+		<div>Loading...</div>
+	{:then posts}
+		<div class="col-lg-3 col-md-4 mt-md-0 mt-sm-2">
+			<div class="row ratio ratio-1x1 mb-4 rounded rounded-3 shadow-sm">
+				<div class="bg-img rounded rounded-3 shadow" style={imgStyle(randImgUrl())} />
+				<div class="row align-content-end p-4 text-white">
+					<h5 class="fw-bold tx-shadow-title">{posts[0].title}</h5>
+				</div>
+			</div>
+			<div class="row ratio ratio-1x1 mt-4 rounded rounded-3 shadow-sm">
+				<div class="bg-img rounded rounded-3" style={imgStyle(randImgUrl())} />
+				<div class="row align-content-end p-4 text-white">
+					<h5 class="fw-bold tx-shadow-title">{posts[1].title}</h5>
+				</div>
 			</div>
 		</div>
-		<div class="row ratio ratio-1x1 mt-4 rounded rounded-3 shadow-sm">
-			<div class="bg-img rounded rounded-3" style={imgStyle(posts[1].image)} />
-			<div class="row align-content-end p-4 text-white">
-				<h4 class="fw-bold tx-shadow-title">{posts[1].title}</h4>
-				<p class="tx-shadow-title">
-					{posts[1].content}
-				</p>
+		<div class="col-lg-6 col-md-8 mt-4 mt-md-0 mt-sm-4">
+			<div class="row ratio ratio-1x1 rounded rounded-3 shadow-sm">
+				<div class="bg-img rounded rounded-3" style={imgStyle(randImgUrl())} />
+				<div class="row align-content-end p-4 text-white">
+					<h5 class="fw-bold tx-shadow-title">{posts[2].title}</h5>
+				</div>
 			</div>
 		</div>
-	</div>
-	<div class="col-lg-6 col-md-8 mt-4 mt-md-0 mt-sm-4">
-		<div class="row ratio ratio-1x1 rounded rounded-3 shadow-sm">
-			<div class="bg-img rounded rounded-3" style={imgStyle(posts[2].image)} />
-			<div class="row align-content-end p-4 text-white">
-				<h4 class="fw-bold tx-shadow-title">{posts[2].title}</h4>
-				<p class="tx-shadow-title">
-					{posts[2].content}
-				</p>
+		<div class="col-lg-3 col-md-12 mt-4 mt-sm-4 mt-md-0 d-none d-lg-block">
+			<div class="row ratio ratio-1x1 mb-4 rounded rounded-3 shadow-sm">
+				<div class="bg-img rounded rounded-3" style={imgStyle(randImgUrl())} />
+				<div class="row align-content-end p-4 text-white">
+					<h5 class="fw-bold tx-shadow-title">{posts[3].title}</h5>
+				</div>
+			</div>
+			<div class="row ratio ratio-1x1 mt-4 rounded rounded-3 shadow-sm">
+				<div class="bg-img rounded rounded-3" style={imgStyle(randImgUrl())} />
+				<div class="row align-content-end p-4 text-white">
+					<h5 class="fw-bold tx-shadow-title">{posts[4].title}</h5>
+				</div>
 			</div>
 		</div>
-	</div>
-	<div class="col-lg-3 col-md-12 mt-4 mt-sm-4 mt-md-0 d-none d-lg-block">
-		<div class="row ratio ratio-1x1 mb-4 rounded rounded-3 shadow-sm">
-			<div class="bg-img rounded rounded-3" style={imgStyle(posts[3].image)} />
-			<div class="row align-content-end p-4 text-white">
-				<h4 class="fw-bold tx-shadow-title">{posts[3].title}</h4>
-				<p class="tx-shadow-title">
-					{posts[3].content}
-				</p>
-			</div>
-		</div>
-		<div class="row ratio ratio-1x1 mt-4 rounded rounded-3 shadow-sm">
-			<div class="bg-img rounded rounded-3" style={imgStyle(posts[4].image)} />
-			<div class="row align-content-end p-4 text-white">
-				<h4 class="fw-bold tx-shadow-title">{posts[4].title}</h4>
-				<p class="tx-shadow-title">
-					{posts[4].content}
-				</p>
-			</div>
-		</div>
-	</div>
+	{:catch error}
+		<p style="color: red">{error.message}</p>
+	{/await}
 </div>
