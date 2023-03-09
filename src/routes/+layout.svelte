03d22186329e3d50a8e590efd91c351976ec13c1
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-03-09 13:34:12 +0300

    bootstrap import deleted

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,6 @@
 <script>
 	import Header from './components/Header.svelte';
 	import './styles.css';
-	import 'bootstrap/dist/css/bootstrap.min.css';
-	import 'bootstrap/dist/js/bootstrap.min.js';
-	import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 </script>
 
 <Header title="SvelteKit Blog" />
